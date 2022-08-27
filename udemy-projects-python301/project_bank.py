2b0a5a170390b22843f80471b61b26f2e1c898e7
et-samurai <feiloveit2017@gmail.com>
2022-08-28 06:13:41 +0800

    Update project_bank.py
    add the class bank to project

--- udemy-projects-python301/project_bank.py
+++ udemy-projects-python301/project_bank.py
@@ -1,1 +1,11 @@
-# this is the testing file for 301
+# create a class hold customers information
+
+class bank:
+    customers=[]
+    customer_id=input("enter your id: ")
+    name=input("enter your full name:  ")
+    def __init__(self,balance=0.00):
+        self.balance=balance
+class customer:
+    
+
