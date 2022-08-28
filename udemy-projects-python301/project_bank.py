c46bd1ff377914d8080d7f058738419d64a3e9f1
et-samurai <feiloveit2017@gmail.com>
2022-08-28 10:58:48 +0800

    files and project301
    create the files for project

--- udemy-projects-python301/project_bank.py
+++ udemy-projects-python301/project_bank.py
@@ -1,11 +1,28 @@
 # create a class hold customers information
 
-class bank:
-    customers=[]
-    customer_id=input("enter your id: ")
-    name=input("enter your full name:  ")
-    def __init__(self,balance=0.00):
-        self.balance=balance
-class customer:
+# class Bank:
+    
+#     def __init__(self,balance=0.00):
+#         self.balance=balance
+
+#     # write the title to the customer.txt
+#     def add_title(s):
+#         with open('customer.txt','a') as file:
+#             file.write(s)
+
+
+# bank=Bank(20.00)
+user=input("type the names of your account: ")
+content=input("type the name you enter")
+
+with open(user,'a') as file:
+    file.write(content)
+    
+    
+
+
+
+
+
     
 
