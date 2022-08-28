1e5e78d35fd784c48934721c6e9a1549e0c3f3a1
pcplusmac <feiloveit2017@gmail.com>
2022-08-28 11:18:45 +0800

    Update project_bank.py

--- udemy-projects-python301/project_bank.py
+++ udemy-projects-python301/project_bank.py
@@ -12,11 +12,27 @@
 
 
 # bank=Bank(20.00)
-user=input("type the names of your account: ")
-content=input("type the name you enter")
 
-with open(user,'a') as file:
+# prompt the user to input the name of the account
+user=input("type the names of your account: ")
+# prompt user to enter the content like to add to account file
+content=input("type the name you enter:  ")
+# capitalize the initial letters 
+content.upper()
+
+# operation on the file
+with open(user,'w') as file:
     file.write(content)
+
+# prompt user to select if like to open the file
+file_opt=input("do you like to open the file now?(y/n)   ")
+
+if file_opt in ['y','n']:
+    if file_opt == 'y':
+        with open(user,'r') as file:
+            print(file.read()) 
+
+
     
     
 
